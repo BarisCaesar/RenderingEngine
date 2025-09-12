2413b48be0f70ab699199d316d027d4448230ca4
BarisCaesar <lowsbaris@gmail.com>
2025-09-12 10:20:14 +0300

    window framework created

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,46 +1,6 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
 
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-	
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(100);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "You pressed the F key my dude.");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "F key is up now.");	
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::string title;
-		title.push_back((char)wParam);
-		SetWindowText(hWnd, title.c_str());
-	}
-		break;
-	
-	case WM_LBUTTONDOWN:
-		
-		break;
-	
-	}
-	
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -48,45 +8,17 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	const auto pClassName = "REngine";
-	//register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	
-	RegisterClassEx(&wc);
+	Window wnd(640, 480, "Selamin");
 
-	// create window class
-	HWND hWnd = CreateWindowEx(
-		0, pClassName, "It works", 
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU , 
-		200, 200, 640, 480, 
-		nullptr, nullptr, hInstance, nullptr
-	);
-	ShowWindow(hWnd, SW_SHOW);
-
-	// message pump
-	BOOL gResult;
 	MSG msg;
-
-	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	BOOL gResult;
+	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
 	{
-	
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	if (gResult == -1)
+	if (gResult == 1)
 	{
 		return -1;
 	}
