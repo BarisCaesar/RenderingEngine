13cfd9dca4445ab24f93a9088146af8430217318
BarisCaesar <lowsbaris@gmail.com>
2025-09-18 09:31:12 +0300

    wheel test

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,6 @@
 
 #include "Window.h"
+#include <sstream>
 
 
 
@@ -19,7 +20,31 @@ int CALLBACK WinMain(
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
-		
+			// test code
+			static int i = 0;
+			while (!wnd.mouse.IsEmpty())
+			{
+				const auto e = wnd.mouse.Read();
+				switch (e.GetType())
+				{
+				case Mouse::Event::Type::WheelUp:
+					i++;
+					{
+						std::ostringstream oss;
+						oss << "Up: " << i;
+						wnd.SetTitle(oss.str());
+					}
+					break;
+				case Mouse::Event::Type::WheelDown:
+					i--;
+					{
+						std::ostringstream oss;
+						oss << "Down: " << i;
+						wnd.SetTitle(oss.str());
+					}
+					break;
+				}
+			}
 		}
 
 		if (gResult == 1)
