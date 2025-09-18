cc42d657b81d28f8b5d0f33f25292b4752705d1c
BarisCaesar <lowsbaris@gmail.com>
2025-09-18 09:30:12 +0300

    proper wheel delta

--- Mouse.h
+++ Mouse.h
@@ -103,6 +103,7 @@ private:
 	void OnWheelUp(int x, int y) noexcept;
 	void OnWheelDown(int x, int y) noexcept;
 	void TrimBuffer() noexcept;
+	void OnWheelDelta(int x, int y, int delta) noexcept;
 private:
 	static constexpr unsigned int bufferSize = 16u;
 	int x;
@@ -110,6 +111,7 @@ private:
 	bool leftIsPressed = false;
 	bool rightIsPressed = false;
 	bool isInWindow = false;
+	int wheelDeltaCarry = 0;
 	std::queue<Event> buffer;
 };
 
