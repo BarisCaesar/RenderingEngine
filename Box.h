b35407bf5b002e6e31cac5be16ab5db6bdd16865
BarisCaeser <barissezer114@gmail.com>
2025-12-10 16:02:12 +0300

    parent class for test objects added

--- Box.h
+++ Box.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "DrawableBase.h"
+#include "TestObject.h"
 
-class Box : public DrawableBase<Box>
+class Box : public TestObject<Box>
 {
 public:
 	Box(Graphics& gifx, std::mt19937& rng,
@@ -12,24 +12,8 @@ public:
 		std::uniform_real_distribution<float>& bdist,
 		DirectX::XMFLOAT3 material
 		);
-	void Update(float dt) noexcept override;
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
 private:
-	// positional
-	float r;
-	float roll = 0.f;
-	float pitch = 0.f;
-	float yaw = 0.f;
-	float theta;
-	float phi;
-	float chi;
-	// speed (delta/s)
-	float droll;
-	float dpitch;
-	float dyaw;
-	float dtheta;
-	float dphi;
-	float dchi;
 	// model transform
 	DirectX::XMFLOAT3X3 mt;
 };
