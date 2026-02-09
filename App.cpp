3fc76a416f1e312726749cd8f62ba8cc9f247ca4
BarisCaeser <lowsbaris@gmail.com>
2026-02-09 14:38:35 +0300

    create normal map shader

--- App.cpp
+++ App.cpp
@@ -109,6 +109,7 @@ void App::DoFrame()
 
 	nano.ShowWindow("Model 1");
 	nano2.ShowWindow("Model 2");
+	plane.SpawnControlWindow(wnd.Gfx());
 
 	
 	// present
--- TestPlane.h
+++ TestPlane.h
@@ -8,7 +8,15 @@ public:
 	void SetPos(DirectX::XMFLOAT3 pos) noexcept;
 	void SetRotation(float xRotation, float yRotation, float zRotation) noexcept;
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
+	void SpawnControlWindow(Graphics& gfx) noexcept;
 private:
+	struct PSMaterialConstant
+	{
+		float specularIntensity = 0.1f;
+		float specularPower = 20.f;
+		BOOL normalMappingEnabled = TRUE;
+		float padding[1];
+	}pmc;
 	DirectX::XMFLOAT3 pos = { 1.0f,1.0f,1.0f };
 	float xRotation = 0.0f;
 	float yRotation = 0.0f;
