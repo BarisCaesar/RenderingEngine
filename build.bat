e0af04ec2fd3c4e26715ca533f9144d78edfa076
BarisCaeser <lowsbaris@gmail.com>
2026-03-09 17:30:06 +0300

    organize project

--- build.bat
+++ build.bat
@@ -1,12 +1,28 @@
 @echo off
-
 cd /d "%~dp0"
 
-if not exist build (
-    mkdir build
+
+for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property displayName') do (
+    set VS_VERSION=%%i
 )
+
+
+set VS_GENERATOR=
+if "%VS_VERSION%"=="2026" set VS_GENERATOR=Visual Studio 18 2026
+if "%VS_VERSION%"=="2022" set VS_GENERATOR=Visual Studio 17 2022
+if "%VS_VERSION%"=="2019" set VS_GENERATOR=Visual Studio 16 2019
+if "%VS_VERSION%"=="2017" set VS_GENERATOR=Visual Studio 15 2017
+
+if "%VS_GENERATOR%"=="" (
+    set VS_GENERATOR=Visual Studio 17 2022
+)
+
+echo Using CMake generator: %VS_GENERATOR%
+
+
+if not exist build mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" ..
+cmake -G "%VS_GENERATOR%" ..
 cmake --build . --config Debug
 
 pause
--- imgui/imgui_impl_dx11.h
+++ imgui/imgui_impl_dx11.h
@@ -9,8 +9,8 @@
 // https://github.com/ocornut/imgui
 
 #pragma once
-#include "../CustomWindows.h"
-#include "../Graphics.h"
+#include "CustomWindows.h"
+#include "Graphics.h"
 #include "imgui.h"
 
 IMGUI_IMPL_API bool     ImGui_ImplDX11_Init(ID3D11Device* device, ID3D11DeviceContext* device_context);
