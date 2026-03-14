1a71a30ad008c4744c14e57eca5016f45d7750a6
BarisCaeser <lowsbaris@gmail.com>
2026-03-14 18:07:23 +0300

    some work on dynamic vertex templates

--- include/RenderQueue/TechniqueProbe.h
+++ include/RenderQueue/TechniqueProbe.h
@@ -11,18 +11,28 @@ public:
 	void SetTechnique(class Technique* pTech_in)
 	{
 		pTech = pTech_in;
+		techIdx++;
 		OnSetTechnique();
 	}
 	void SetStep(class Step* pStep_in)
 	{
 		pStep = pStep_in;
+		stepIdx++;
 		OnSetStep();
 	}
-	virtual bool VisitBuffer(class DynamicConstBuf::Buffer&) = 0;
+	virtual bool VisitBuffer(class DynamicConstBuf::Buffer& buf)
+	{
+		bufIdx++;
+		return OnVisitBuffer(buf);
+	}
 protected:
 	virtual void OnSetTechnique() {}
 	virtual void OnSetStep() {}
+	virtual bool OnVisitBuffer(class DynamicConstBuf::Buffer&) = 0;
 protected:
 	class Technique* pTech;
 	class Step* pStep;
+	size_t techIdx = std::numeric_limits<size_t>::max();
+	size_t stepIdx = std::numeric_limits<size_t>::max();
+	size_t bufIdx = std::numeric_limits<size_t>::max();
 };
