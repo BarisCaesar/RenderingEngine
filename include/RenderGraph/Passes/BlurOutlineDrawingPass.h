5d74ddbea66915c8f68b39308500cb13defbd4cb
BarisCaeser <lowsbaris@gmail.com>
2026-05-23 13:30:01 +0300

    fix outline silhoutte clear bug

--- include/RenderGraph/Passes/BlurOutlineDrawingPass.h
+++ include/RenderGraph/Passes/BlurOutlineDrawingPass.h
@@ -29,5 +29,10 @@ namespace RenderGraph
 			AddBind(Blender::Resolve(gfx, false));
 			RegisterSource(DirectBindableSource<Bind::RenderTarget>::Make("scratchOut", renderTarget));
 		}
+		void Execute(Graphics& gfx) const noxnd override
+		{
+			renderTarget->Clear(gfx);
+			RenderQueuePass::Execute(gfx);
+		}
 	};
 }
