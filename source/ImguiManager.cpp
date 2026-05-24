9584a308acce9c32464841c9fbfecbfeeaa7c18d
BarisCaeser <lowsbaris@gmail.com>
2026-05-24 16:28:45 +0300

    imgui window default config

--- source/ImguiManager.cpp
+++ source/ImguiManager.cpp
@@ -1,10 +1,16 @@
 #include "ImguiManager.h"
 #include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/imgui_impl_dx11.h"
+#include <filesystem>
+#include "RUtil.h"
 
 ImguiManager::ImguiManager()
 {
+	namespace fs = std::filesystem;
+	auto defaultConfigPath = FindFileInProject("imgui_default.ini");
+	if (!fs::exists("imgui.ini") && fs::exists(defaultConfigPath))
+	{
+		fs::copy_file(defaultConfigPath, "imgui.ini");
+	}
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGui::StyleColorsDark();
