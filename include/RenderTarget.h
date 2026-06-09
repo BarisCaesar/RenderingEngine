ea2ccf5d2d65d833646560821c6c110ed56a89e8
BarisCaeser <lowsbaris@gmail.com>
2026-06-09 16:06:21 +0300

    improvements for rendering to cubemap

--- include/RenderTarget.h
+++ include/RenderTarget.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Bindable.h"
 #include "BufferResource.h"
+#include <optional>
 
 class Graphics;
 class Surface;
@@ -22,7 +23,7 @@ namespace Bind
 	private:
 		void BindAsBuffer(Graphics& gfx, ID3D11DepthStencilView* pDepthStencilView) noxnd;
 	protected:
-		RenderTarget(Graphics& gfx, ID3D11Texture2D* pTexture);
+		RenderTarget(Graphics& gfx, ID3D11Texture2D* pTexture, std::optional<UINT> face);
 		RenderTarget(Graphics& gfx, UINT width, UINT height);
 		UINT width;
 		UINT height;
@@ -48,7 +49,7 @@ namespace Bind
 	public:
 		void Bind(Graphics& gfx) noxnd override;
 	private:
-		OutputOnlyRenderTarget(Graphics& gfx, ID3D11Texture2D* pTexture);
+		OutputOnlyRenderTarget(Graphics& gfx, ID3D11Texture2D* pTexture, std::optional<UINT> face = {});
 	};
 }
 
