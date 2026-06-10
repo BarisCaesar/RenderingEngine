4c5056cb97ebdc9b1af207464789f4a3d0f58fa8
BarisCaeser <lowsbaris@gmail.com>
2026-06-10 21:14:45 +0300

    face depth calculation

--- shaders/vertex/Shadow_VS.hlsl
+++ shaders/vertex/Shadow_VS.hlsl
@@ -1,18 +1,7 @@
 #include "Transform.hlsl"
 
-struct Output
+float4 main(float3 pos : Position) : SV_Position
 {
-    float4 pos : SV_Position;
-    float4 viewPos : Position;
-};
-
-Output main(float3 pos : Position)
-{
-    Output output;
-    output.pos = mul(float4(pos, 1.0f), modelViewProj);
-    output.viewPos.xyz = mul(float4(pos, 1.f), modelView).xyz;
-    output.viewPos.w = output.pos.z;
-
-    return output;
+    return mul(float4(pos, 1.0f), modelViewProj);
 }
 
